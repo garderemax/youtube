82f7a8990bf8070e6023b47f3382e9ae5a70cc26
max-nerazim <garderemax@gmail.com>
2022-11-16 16:57:57 +0100

    first iteration

---
+++ main.py
@@ -0,0 +1,39 @@
+import sys
+import json
+import requests
+from bs4 import BeautifulSoup
+
+
+def scrap(videoId):
+    url = "https://www.youtube.com/watch?v=" + videoId + "&format=json"
+    embedUrl = "http://www.youtube.com/oembed?url=" + url + "&format=json"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content,'html.parser')
+
+
+    result = {}
+
+    result["title"] = soup.find("meta", itemprop="name")["content"]
+    result["authorName"] = soup.find("span", itemprop="author").next.next["content"]
+    result["viewCount"] = soup.find("meta", itemprop="interactionCount")["content"]
+    result["description"] = soup.find("meta", itemprop="description")["content"]
+    result["descriptionLinks"] = []
+    result["id"] = videoId
+    result["comments"] = []
+
+    return result
+
+def main():
+    if len(sys.argv) != 5:
+        return
+    inputFilePath = sys.argv[2]
+    outputFilePath = sys.argv[4]
+
+    input = json.load(open(inputFilePath))
+    output = [scrap(videoId) for videoId in input["videos_id"]]
+    outputFile = open(outputFilePath, "w")
+    outputFile.write(json.dumps(output, indent=4))
+    outputFile.close()
+
+if __name__ == "__main__":
+    main()
